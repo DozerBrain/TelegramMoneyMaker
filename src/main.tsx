0b1e3d31ea02c817286117e03ee4fb3c337bc307
DozerBrain <dozer97brain@gmail.com>
2025-11-10 13:43:19 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css"; // 👈 VERY IMPORTANT
+
+// 🧩 Styles (import all before rendering)
+import "./index.css";
+import "./styles/global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
