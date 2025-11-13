8433cc30b0721248212a48eb9c938da7f0c1f97f
DozerBrain <dozer97brain@gmail.com>
2025-11-13 12:22:32 +0300

    Update firabase.ts

--- src/lib/firabase.ts
+++ src/lib/firabase.ts
@@ -1,16 +1,15 @@
-// src/lib/firebase.ts
-import { initializeApp, getApps, getApp } from "firebase/app";
+import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBGSR1l1ocnZu7i72rK_C51ML_eI87X6Q",
   authDomain: "moneymaker-a669c.firebaseapp.com",
   projectId: "moneymaker-a669c",
-  storageBucket: "moneymaker-a669c.appspot.com",
+  storageBucket: "moneymaker-a669c.firebasestorage.app",
   messagingSenderId: "352896227263",
   appId: "1:352896227263:web:3d998d8a2f22f2d7a2730a",
   databaseURL: "https://moneymaker-a669c-default-rtdb.firebaseio.com",
 };
 
-export const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 export const db = getDatabase(app);
